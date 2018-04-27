b14f954f3389e826a2a851309d3e2758b67da899
sireBlack <dblacktin@gmail.com>
2018-04-27 18:33:55 +0100

    completed contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <h1>CONTACT</h1>
 {!! Form::open(['url' => 'foo/bar']) !!}
-    
+    <div class="form-group">
+        {{Form::label('name', 'Name')}}
+        {{Form::text('Enter Name', '', ['class'=>'form-control col-md-6', 'placeholder'=>'Enter Name'])}}
+    </div>
+    <div class="form-group">
+        {{Form::label('email', 'E-Mail Address')}}
+        {{Form::text('email', '', ['class'=>'form-control col-md-6', 'placeholder'=>'Enter E-mail address'])}}
+    </div>
+    <div class="form-group">
+        {{Form::label('message', 'Message')}}
+        {{Form::textarea('message', '', ['class'=>'form-control col-md-6', 'placeholder'=>'Enter Message'])}}
+    </div>
 {!! Form::close() !!}
 @endsection
